12998a365a22e05e93d9fa620f98011790c8eb5e
VictorCR18 <cavalcantevictor2003@gmail.com>
2023-02-10 13:17:47 -0300

    comencando tradocao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,23 @@
 import "./styles.css";
 import Union from "../../assets/Union.svg";
 
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
 export function Header() {
+  const {
+    t,
+    i18n: { changeLanguage, language },
+  } = useTranslation();
+
+  const [currentLanguage, setCurrentLanguage] = useState(language);
+
+  const handleChangeLanguage = (event: any) => {
+    const newLanguage = currentLanguage === "en" ? "pt" : "en";
+    changeLanguage(newLanguage);
+    setCurrentLanguage(newLanguage);
+  };
+
   return (
     <div className="main-header">
       <div className="logo">
@@ -14,17 +30,20 @@ export function Header() {
           <label className="label-color">#</label>home
         </a>
         <a className="label-header">
-          <label className="label-color">#</label>works
+          <label className="label-color">#</label>
+          {t("navWorks")}
         </a>
         <a className="label-header">
-          <label className="label-color">#</label>about-me
+          <label className="label-color">#</label>{t("navAbout")}
         </a>
         <a className="label-header">
-          <label className="label-color">#</label>contacts
+          <label className="label-color">#</label>{t("navContacts")}
         </a>
-        <select className="select" name="language">
+        <select id="mySelect" onChange={handleChangeLanguage} className="select" name="language">
           <option value="EN">EN</option>
-          <option value="PT-Br">PT</option>
+          <option id="langPT" value="PT">
+            PT
+          </option>
         </select>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import "./styles/global.css";
 import { Home } from "./pages/Home";
 import { Media } from "./components/Media";
 
+import './i18n/i18n.ts';
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Media />
